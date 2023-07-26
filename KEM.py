237e0323aa12f2a77199f00247fda1e83f9d9558
felisevan <felisevan@protonmail.com>
2023-07-26 20:50:05 +0800

    code & docstring format

--- KEM.py
+++ KEM.py
@@ -1,19 +1,20 @@
 from modes import KEM_IDS
 
-class KEM(object):
+class KEM:
     """
     class of HPKE's KEM compomnent
     """
 
     def __init__(self,KEM_ID: KEM_IDS):
         """
-        parameters:
-                Nsecret: length of shared secret produced by KEM
-                Nenc: length of encapsulated key produced by KEM
-                Npk: length of encoded public key for KEM
-                Nsk: length of encoded private key for KEM
+        :param Nsecret: length of shared secret produced by KEM
+        :param Nenc: length of encapsulated key produced by KEM
+        :param Npk: length of encoded public key for KEM
+        :param Nsk: length of encoded private key for KEM
+        :return: 
         """
         
     @property
     def KEM_ID(self):
+        # TODO: docstring
         pass
