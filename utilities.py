c6820444b483b40ee8ce64fb098823c85dff069b
14MBD4 <140333308+14MBD4@users.noreply.github.com>
2023-07-26 16:41:42 -0400

    small changes need to implement

---
+++ utilities.py
@@ -0,0 +1,15 @@
+from collections.abc import Iterable
+
+
+def concat(*args: bytes) -> bytes:
+    rt = b""
+    return rt.join(args)
+
+def I2OSP():
+    pass
+
+def OS2IP():
+    pass
+
+# def suite_id
+
