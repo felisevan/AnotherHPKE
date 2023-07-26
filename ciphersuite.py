dfaf3df8b4bf295556d5238f3ccb123d30df7ad5
felisevan <felisevan@protonmail.com>
2023-07-26 20:50:05 +0800

    code & docstring format

--- ciphersuite.py
+++ ciphersuite.py
@@ -1,18 +1,20 @@
 from modes import *
 import KEM,KDF,AEAD
 
-class ciphersuite(object):
+class ciphersuite:
     """
     HPKE cipher suite composed by triple (KEM,KDF,AEAD)
     """
 
     def __init__(self, KEM_ID: KEM_IDS, KDF_ID: KEM_IDS, AEAD_ID: AEAD_IDS):
+        # TODO: docstring
         self.__kem_id = KEM_ID
         self.__KDF_id = KDF_ID
         self.__aead_id = AEAD_ID
 
     @classmethod
     def initial(self, KEM_ID: KEM_IDS, KDF_ID: KEM_IDS, AEAD_ID: AEAD_IDS):
+        # TODO: docstring
         self.__kem_id = KEM(KEM_ID)
         self.__kdf_id = KDF(KDF_ID)
         self.__aead_id  = AEAD(AEAD_ID)
