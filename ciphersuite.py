a3ca39e272f1ee996a21e7cd2701b2fed6d2e3dd
felisevan <felisevan@protonmail.com>
2023-07-27 19:49:29 +0800

    (refactor) rename constants.py

--- ciphersuite.py
+++ ciphersuite.py
@@ -1,24 +1,3 @@
-import AEAD
-import HKDF111
-import KEM
-from modes import *
+from collections import namedtuple
 
-
-class ciphersuite:
-    """
-    HPKE cipher suite composed by triple (KEM,KDF,AEAD)
-    """
-
-    def __init__(self, KEM_ID: KEM_IDS, KDF_ID: KEM_IDS, AEAD_ID: AEAD_IDS):
-        # TODO: docstring
-        self.__kem_id = KEM_ID
-        self.__KDF_id = KDF_ID
-        self.__aead_id = AEAD_ID
-
-    @classmethod
-    def initial(self, KEM_ID: KEM_IDS, KDF_ID: KEM_IDS, AEAD_ID: AEAD_IDS):
-        # TODO: docstring
-        self.__kem_id = KEM(KEM_ID)
-        self.__kdf_id = HKDF111(KDF_ID)
-        self.__aead_id = AEAD(AEAD_ID)
-        pass
+CipherSuite = namedtuple("CipherSuite", ["KEM_ID", "KDF_ID", "AEAD_ID"])
