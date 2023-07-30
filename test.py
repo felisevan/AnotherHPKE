c9fd763105a9c49018134232596c7d55d0570f17
felisevan <felisevan@protonmail.com>
2023-07-31 01:09:51 +0800

    (test) partially done

--- test.py
+++ test.py
@@ -1,19 +1,79 @@
+import json
+from pprint import pprint
 from constants import MODE_IDS, KEM_IDS, KDF_IDS, AEAD_IDS
-from ContextFactory import ContextFactory
-from KEM import DhKemX25519HkdfSha256
-from utilities import I2OSP
-from cryptography.hazmat.primitives.asymmetric.x25519 import X25519PrivateKey, X25519PublicKey
+from Context import ContextFactory
+
+
+def load_test_vector():
+    with open("test-vectors.json") as f:
+        return json.load(f)
+
+
+def bfh(x):
+    return bytes.fromhex(x)
+
+
+def test_asym_key(kem, ikm, sk, pk):
+    pri, pub = kem.derive_key_pair(ikm)
+    sk = kem.deserialize_private_key(sk)
+    pk = kem.deserialize_public_key(pk)
+    assert kem.serialize_private_key(pri) == kem.serialize_private_key(sk)
+    assert kem.serialize_public_key(pub) == kem.serialize_public_key(pk)
+
+
+def test(x):
+    aead_id = AEAD_IDS(x['aead_id'])
+    kdf_id = KDF_IDS(x['kdf_id'])
+    kem_id = KEM_IDS(x['kem_id'])
+    c = ContextFactory(kem_id, kdf_id, aead_id)
+
+    ikmR = bfh(x['ikmR'])
+    skRm = bfh(x['skRm'])
+    pkRm = bfh(x['pkRm'])
+    test_asym_key(c.kem, ikmR, skRm, pkRm)
+
+    ikmE = bfh(x['ikmE'])
+    skEm = bfh(x['skEm'])
+    pkEm = bfh(x['pkEm'])
+    test_asym_key(c.kem, ikmE, skEm, pkEm)
+
+    info = bfh(x['info'])  # message to be encrypted
+    enc = bfh(x['enc'])  # should be same as pkEm
+    shared_secret = bfh(x['shared_secret'])  # crypt state
+    key_schedule_context = bfh(x['key_schedule_context'])
+    secret = bfh(x['secret'])
+    key = bfh(x['key'])
+    base_nonce = bfh(x['base_nonce'])
+    exporter_secret = bfh(x['exporter_secret'])
+    mode_id = MODE_IDS(x['mode'])
+
+    match mode_id:
+        case MODE_IDS.MODE_AUTH_PSK:
+            ikmS = bfh(x['ikmS'])
+            skSm = bfh(x['skSm'])
+            pkSm = bfh(x['pkEm'])
+            test_asym_key(c.kem, ikmS, skSm, pkSm)
+
+            psk = bfh(x['psk'])
+            psk_id = bfh(x['psk_id'])
+
+        case MODE_IDS.MODE_AUTH:
+            ikmS = bfh(x['ikmS'])
+            skSm = bfh(x['skSm'])
+            pkSm = bfh(x['pkEm'])
+            test_asym_key(c.kem, ikmS, skSm, pkSm)
+
+        case MODE_IDS.MODE_PSK:
+            psk = bfh(x['psk'])
+            psk_id = bfh(x['psk_id'])
+
+        case MODE_IDS.MODE_BASE:
+            print()
+
+        case _:
+            raise NotImplementedError
+
+
 if __name__ == '__main__':
-    info = bytes.fromhex("4f6465206f6e2061204772656369616e2055726e")
-    ikmR = bytes.fromhex("4b16221f3b269a88e207270b5e1de28cb01f847841b344b8314d6a622fe5ee90")
-    aad = bytes.fromhex("436f756e742d30")
-    nonce = bytes.fromhex("99d8b5c54669807e9fc70df1")
-    pt = bytes.fromhex("4265617574792069732074727574682c20747275746820626561757479")
-    psk = bytes.fromhex("0247fd33b913760fa1fa51e1892d9f307fbe65eb171e8132c2af18555a738b82")
-    psk_id = bytes.fromhex("456e6e796e20447572696e206172616e204d6f726961")
-    pri, pub = DhKemX25519HkdfSha256().derive_key_pair(ikmR)
-    skSm = bytes.fromhex("fc1c87d2f3832adb178b431fce2ac77c7ca2fd680f3406c77b5ecdf818b119f4")
-    spri = X25519PrivateKey.from_private_bytes(skSm)
-    enc, context = ContextFactory(KEM_IDS.DHKEM_X25519_HKDF_SHA256, KDF_IDS.HKDF_SHA256, AEAD_IDS.AES_128_GCM).SetupAuthPSKS(pub, info, psk, psk_id,spri)
-    l = context.seal(aad, pt)
-    print(l.hex())
+    test_vectors = load_test_vector()
+    pprint(test_vectors[3])
