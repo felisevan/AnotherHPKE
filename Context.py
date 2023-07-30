1d23c9deb083b796a09e3ddead9e0c096f605685
felisevan <felisevan@protonmail.com>
2023-07-30 20:26:13 +0800

    (refactor) merge ContextFactory

--- Context.py
+++ Context.py
@@ -1,8 +1,8 @@
-from utilities import xor_bytes
-from utilities import I2OSP
-from KDF import AbstractHkdf
-from AEAD import AbstractAead
-
+from utilities import concat, I2OSP, xor_bytes
+from constants import MODE_IDS, KEM_IDS, KDF_IDS, AEAD_IDS
+from KDF import KdfFactory, AbstractHkdf
+from KEM import KemFactory
+from AEAD import AeadFactory, AbstractAead
 
 class MessageLimitReachedError(Exception):
     pass
@@ -101,3 +101,129 @@ class ContextRecipient(AbstractContext):
 
     def seal(self, aad, pt):
         raise NotImplementedError("Invalid in export-only")
+
+
+class ContextFactory:
+    def __init__(self, kem: KEM_IDS, kdf: KDF_IDS, aead: AEAD_IDS):
+        self.suite_id = concat(
+            b"HPKE",
+            I2OSP(kem, 2),
+            I2OSP(kdf, 2),
+            I2OSP(aead, 2)
+        )
+        self._default_psk = b""
+        self._default_psk_id = b""
+        self.aead = AeadFactory.new(aead)
+        self.kem = KemFactory.new(kem)
+        self.kdf = KdfFactory.new(kdf)
+
+    def _verify_psk_inputs(self, mode, psk=b"", psk_id=b""):
+        got_psk = (psk != self._default_psk)
+        got_psk_id = (psk_id != self._default_psk_id)
+        if got_psk != got_psk_id:
+            raise Exception("Inconsistent PSK inputs")
+
+        if got_psk and (mode in [MODE_IDS.MODE_BASE, MODE_IDS.MODE_AUTH]):
+            raise Exception("PSK input provided when not needed")
+        if (not got_psk) and (mode in [MODE_IDS.MODE_PSK, MODE_IDS.MODE_AUTH_PSK]):
+            raise Exception("Missing required PSK input")
+
+    def _key_schedule(self, mode, shared_secret, info, psk, psk_id, role):
+        self._verify_psk_inputs(mode, psk, psk_id)
+
+        psk_id_hash = self.kdf.labeled_extract(b"", b"psk_id_hash", psk_id, suite_id=self.suite_id)
+        info_hash = self.kdf.labeled_extract(b"", b"info_hash", info, suite_id=self.suite_id)
+        key_schedule_context = concat(I2OSP(mode, 1), psk_id_hash, info_hash)
+
+        secret = self.kdf.labeled_extract(shared_secret, b"secret", psk, suite_id=self.suite_id)
+
+        key = self.kdf.labeled_expand(secret, b"key", key_schedule_context, self.aead.Nk, suite_id=self.suite_id)
+        base_nonce = self.kdf.labeled_expand(secret, b"base_nonce", key_schedule_context, self.aead.Nn,
+                                             suite_id=self.suite_id)
+        exporter_secret = self.kdf.labeled_expand(secret, b"exp", key_schedule_context, self.kdf.Nh,
+                                                  suite_id=self.suite_id)
+
+        if role == "sender":
+            return ContextSender(
+                suite_id=self.suite_id,
+                kdf=self.kdf,
+                aead=self.aead,
+                key=key,
+                base_nonce=base_nonce,
+                seq=0,
+                exporter_secret=exporter_secret
+            )
+        elif role == "recipient":
+            return ContextRecipient(
+                suite_id=self.suite_id,
+                kdf=self.kdf,
+                aead=self.aead,
+                key=key,
+                base_nonce=base_nonce,
+                seq=0,
+                exporter_secret=exporter_secret
+            )
+        elif role == "exporter":
+            return ContextExportOnly(
+                suite_id=self.suite_id,
+                kdf=self.kdf,
+                exporter_secret=exporter_secret
+            )
+        else:
+            raise NotImplementedError
+
+    def key_schedule_sender(self, mode, shared_secret, info, psk, psk_id):
+        return self._key_schedule(mode, shared_secret, info, psk, psk_id, "sender")
+
+    def key_schedule_recipient(self, mode, shared_secret, info, psk, psk_id):
+        return self._key_schedule(mode, shared_secret, info, psk, psk_id, "recipient")
+
+    def key_schedule_exporter(self, mode, shared_secret, info, psk, psk_id):
+        return self._key_schedule(mode, shared_secret, info, psk, psk_id, "exporter")
+
+    def SetupBaseS(self, pkR, info, skE: bytes = None, pkE: bytes = None):
+        shared_secret, enc = self.kem.encap(pkR, skE, pkE)
+        return enc, self.key_schedule_sender(MODE_IDS.MODE_BASE, shared_secret, info,
+                                             self._default_psk, self._default_psk_id)
+
+    def SetupBaseR(self, enc, skR, info):
+        shared_secret = self.kem.decap(enc, skR)
+        return self.key_schedule_recipient(MODE_IDS.MODE_BASE, shared_secret, info,
+                                           self._default_psk, self._default_psk_id)
+
+    def SetupPSKS(self, pkR, info, psk, psk_id, skE: bytes = None, pkE: bytes = None):
+        if len(psk) < 32:
+            raise Exception
+        shared_secret, enc = self.kem.encap(pkR, skE, pkE)
+        return enc, self.key_schedule_sender(MODE_IDS.MODE_PSK, shared_secret, info,
+                                             psk, psk_id)
+
+    def SetupPSKR(self, enc, skR, info, psk, psk_id):
+        if len(psk) < 32:
+            raise Exception
+        shared_secret = self.kem.decap(enc, skR)
+        return self.key_schedule_recipient(MODE_IDS.MODE_PSK, shared_secret, info, psk, psk_id)
+
+    def SetupAuthS(self, pkR, info, skS, skE: bytes = None, pkE: bytes = None):
+        shared_secret, enc = self.kem.auth_encap(pkR, skS, skE, pkE)
+        return enc, self.key_schedule_sender(MODE_IDS.MODE_AUTH, shared_secret, info,
+                                             self._default_psk, self._default_psk_id)
+
+    def SetupAuthR(self, enc, skR, info, pkS):
+        shared_secret = self.kem.auth_decap(enc, skR, pkS)
+        return self.key_schedule_recipient(MODE_IDS.MODE_AUTH, shared_secret, info,
+                                           self._default_psk, self._default_psk_id)
+
+    def SetupAuthPSKS(self, pkR, info, psk, psk_id, skS, skE: bytes = None, pkE: bytes = None):
+        if len(psk) < 32:
+            raise Exception
+        shared_secret, enc = self.kem.auth_encap(pkR, skS, skE, pkE)
+        return enc, self.key_schedule_sender(MODE_IDS.MODE_AUTH_PSK, shared_secret, info,
+                                             psk, psk_id)
+
+    def SetupAuthPSKR(self, enc, skR, info, psk, psk_id, pkS):
+        if len(psk) < 32:
+            raise Exception
+        shared_secret = self.kem.auth_decap(enc, skR, pkS)
+        return self.key_schedule_recipient(MODE_IDS.MODE_AUTH_PSK, shared_secret, info,
+                                           psk, psk_id)
