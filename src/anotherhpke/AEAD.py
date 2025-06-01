45811e495fe23903a74f4f5206961b5c09a9ade7
felisevan <felisevan@protonmail.com>
2025-06-02 02:40:10 +0800

    (refactor) refactor AEAD using Protocol pattern

--- src/anotherhpke/AEAD.py
+++ src/anotherhpke/AEAD.py
@@ -1,85 +1,76 @@
-from abc import ABC, abstractmethod
-from typing import Callable
+from collections.abc import Callable
+from typing import Protocol
 
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM, ChaCha20Poly1305
 
 from .constants import AeadIds
 
 
-class AbstractAead(ABC):
+# 2. Define the main AEAD Protocol
+class AeadProtocol(Protocol):
     """
-    Abstract class of AEAD with declaring methods.
+    Protocol defining the interface for an AEAD cipher.
     """
 
     @property
-    @abstractmethod
     def id(self) -> AeadIds:
-        """
-        The AEAD id.
-        """
-        raise NotImplementedError
+        """The AEAD id."""
+        ...
 
     @property
-    @abstractmethod
     def Nk(self) -> int:
-        """
-        The length in bytes of a key for this algorithm.
-        """
-        raise NotImplementedError
+        """The length in bytes of a key for this algorithm."""
+        ...
 
     @property
-    @abstractmethod
     def Nn(self) -> int:
-        """
-        The length in bytes of a nonce for this algorithm.
-        """
-        raise NotImplementedError
+        """The length in bytes of a nonce for this algorithm."""
+        ...
 
     @property
-    @abstractmethod
     def Nt(self) -> int:
-        """
-        The length in bytes of the authentication tag for this algorithm.
-        """
-        raise NotImplementedError
+        """The length in bytes of the authentication tag for this algorithm."""
+        ...
 
     @property
-    @abstractmethod
     def _algorithm(self) -> Callable:
         """
-        The underlying AEAD cipher.
+        A callable that takes a key and returns an AEAD cipher instance.
         """
-        raise NotImplementedError
+        ...
 
     def seal(self, key: bytes, nonce: bytes, aad: bytes | None, pt: bytes) -> bytes:
         """
         Encrypts plaintext `pt` and authenticates optional associated data `aad` using `key` and `nonce`.
-
-        :param key: A symmetric key.
-        :param nonce: A nonce value.
-        :param aad: Additional data that should be authenticated with the key, but does not need to be encrypted.
-        :param pt: A Plaintext.
-        :return: The ciphertext.
         """
-
-        cipher = self._algorithm(key)
-        return cipher.encrypt(nonce=nonce, data=pt, associated_data=aad)
+        ...
 
     def open(self, key: bytes, nonce: bytes, aad: bytes | None, ct: bytes) -> bytes:
         """
         Decrypts ciphertext `ct` and authenticates optional associated data `aad` using `key` and `nonce`.
-
-        :param key: A symmetric key.
-        :param nonce: A nonce value.
-        :param aad: Additional data to authenticate.
-        :param ct: A Ciphertext.
-        :return: The plaintext.
         """
-        cipher = self._algorithm(key)
-        return cipher.decrypt(nonce=nonce, data=ct, associated_data=aad)
+        ...
 
 
-class AeadAes256Gcm(AbstractAead):
+class AeadOperationMixin:
+    """
+    Mixin providing the seal and open operations for AEAD ciphers that conform to a part of the AeadProtocol.
+    """
+
+    def seal(
+        self: AeadProtocol, key: bytes, nonce: bytes, aad: bytes | None, pt: bytes
+    ) -> bytes:
+        cipher_instance = self._algorithm(key)
+        return cipher_instance.encrypt(nonce=nonce, data=pt, associated_data=aad)
+
+    def open(
+        self: AeadProtocol, key: bytes, nonce: bytes, aad: bytes | None, ct: bytes
+    ) -> bytes:
+        cipher_instance = self._algorithm(key)
+        return cipher_instance.decrypt(nonce=nonce, data=ct, associated_data=aad)
+
+
+class AeadAes256Gcm(AeadOperationMixin):
     @property
     def id(self) -> AeadIds:
         return AeadIds.AES_256_GCM
@@ -97,11 +88,13 @@ class AeadAes256Gcm(AbstractAead):
         return 16
 
     @property
-    def _algorithm(self) -> Callable:
+    def _algorithm(
+        self,
+    ) -> type[AESGCM]:
         return AESGCM
 
 
-class AeadAes128Gcm(AbstractAead):
+class AeadAes128Gcm(AeadOperationMixin):
     @property
     def id(self) -> AeadIds:
         return AeadIds.AES_128_GCM
@@ -119,11 +112,11 @@ class AeadAes128Gcm(AbstractAead):
         return 16
 
     @property
-    def _algorithm(self) -> Callable:
+    def _algorithm(self) -> type[AESGCM]:
         return AESGCM
 
 
-class AeadChaCha20Poly1305(AbstractAead):
+class AeadChaCha20Poly1305(AeadOperationMixin):
     @property
     def id(self) -> AeadIds:
         return AeadIds.ChaCha20Poly1305
@@ -141,36 +134,40 @@ class AeadChaCha20Poly1305(AbstractAead):
         return 16
 
     @property
-    def _algorithm(self) -> Callable:
+    def _algorithm(
+        self,
+    ) -> type[ChaCha20Poly1305]:
         return ChaCha20Poly1305
 
 
-class AeadExportOnly(AbstractAead):
+class AeadExportOnly:
     @property
     def id(self) -> AeadIds:
         return AeadIds.Export_only
 
     @property
     def Nk(self) -> int:
-        raise NotImplementedError("Export only")
+        raise NotImplementedError("Export only: Nk not applicable")
 
     @property
     def Nn(self) -> int:
-        raise NotImplementedError("Export only")
+        raise NotImplementedError("Export only: Nn not applicable")
 
     @property
     def Nt(self) -> int:
-        raise NotImplementedError("Export only")
+        raise NotImplementedError("Export only: Nt not applicable")
 
     @property
-    def _algorithm(self) -> Callable:
-        raise NotImplementedError("Export only")
+    def _algorithm(
+        self,
+    ) -> Callable:
+        raise NotImplementedError("Export only: _algorithm not applicable")
 
-    def seal(self, key: None, nonce: None, aad: None, pt: None) -> None:
-        raise NotImplementedError("Export only")
+    def seal(self, key: bytes, nonce: bytes, aad: bytes | None, pt: bytes) -> bytes:
+        raise NotImplementedError("Export only: seal operation not supported")
 
-    def open(self, key: None, nonce: None, aad: None, ct: None) -> None:
-        raise NotImplementedError("Export only")
+    def open(self, key: bytes, nonce: bytes, aad: bytes | None, ct: bytes) -> bytes:
+        raise NotImplementedError("Export only: open operation not supported")
 
 
 class AeadFactory:
@@ -179,14 +176,12 @@ class AeadFactory:
     """
 
     @classmethod
-    def new(
-        cls, aead_id: AeadIds
-    ) -> AeadAes128Gcm | AeadAes256Gcm | AeadChaCha20Poly1305 | AeadExportOnly:
+    def new(cls, aead_id: AeadIds) -> AeadProtocol:
         """
         Create an instance of corresponding AEAD.
 
         :param aead_id: AEAD id.
-        :return: An instance of AEAD.
+        :return: An instance conforming to AeadProtocol.
         """
         match aead_id:
             case AeadIds.AES_128_GCM:
@@ -198,4 +193,6 @@ class AeadFactory:
             case AeadIds.Export_only:
                 return AeadExportOnly()
             case _:
-                raise NotImplementedError
+                raise NotImplementedError(
+                    f"AEAD ID {aead_id} not implemented in factory."
+                )
